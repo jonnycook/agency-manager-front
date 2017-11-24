9f7a4aacfc11838d69a629422fcbe8dcf2d791f8
Jonathan Cook <jonathan@stratosphere.digital>
2017-11-24 13:03:39 -0500

    .

--- src/WorkDay.js
+++ src/WorkDay.js
@@ -12,17 +12,26 @@ export class WorkDay extends XComponent {
 	}
 	xRender() {
 		var end = this.props.workDay.end || new Date();
-		var timeSpent = db.work_log_entries.filter((workLogEntry) => {
+		var timeSpent = Math.floor(db.work_log_entries.filter((workLogEntry) => {
 			return workLogEntry.start.isBetween(this.props.workDay.begin, end);
 		}).reduce((timeSpent, entry) => {
 			return timeSpent + (entry.end || new Date()).getTime() - entry.start.getTime();
-		}, 0);
+		}, 0)/1000);
 		return (
 			<div>
 				<div>Begin: <PropertyField object={this.props.workDay} property="begin" type="datetime" /></div>
 				<div>End: <PropertyField object={this.props.workDay} property="end" type="datetime" /></div>
+				<div>Time: <PropertyField object={this.props.workDay} property="time" type="duration" /></div>
 
-				{juration.stringify(Math.floor(timeSpent/1000))}
+				{juration.stringify(timeSpent)}
+
+				{this.props.workDay.time &&
+					<div>
+						<div>Time left: {juration.stringify(this.props.workDay.time - timeSpent)}</div>
+						<div>Time When Finished: {new Date(new Date().getTime() + (this.props.workDay.time - timeSpent)*1000).toString()}</div>
+
+					</div>
+				}
 			</div>
 		);
 	}
