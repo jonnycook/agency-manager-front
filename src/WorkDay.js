2906e700c1aa30720520d2ebc517129a20ce6d1a
Jonathan Cook <jonathan@stratosphere.digital>
2017-11-24 15:55:14 -0500

    .

--- src/WorkDay.js
+++ src/WorkDay.js
@@ -17,19 +17,21 @@ export class WorkDay extends XComponent {
 		}).reduce((timeSpent, entry) => {
 			return timeSpent + (entry.end || new Date()).getTime() - entry.start.getTime();
 		}, 0)/1000);
+		var timeSinceStarted = Math.floor((new Date().getTime() - this.props.workDay.begin.getTime())/1000);
 		return (
 			<div>
 				<div>Begin: <PropertyField object={this.props.workDay} property="begin" type="datetime" /></div>
 				<div>End: <PropertyField object={this.props.workDay} property="end" type="datetime" /></div>
 				<div>Time: <PropertyField object={this.props.workDay} property="time" type="duration" /></div>
 
-				{juration.stringify(timeSpent)}
+				<div>{juration.stringify(timeSpent)}</div>
+				<div>Time Since Started: {juration.stringify(timeSinceStarted)}</div>
+				<div>Other Time: {juration.stringify(timeSinceStarted - timeSpent)}</div>
 
 				{this.props.workDay.time &&
 					<div>
 						<div>Time left: {juration.stringify(this.props.workDay.time - timeSpent)}</div>
 						<div>Time When Finished: {new Date(new Date().getTime() + (this.props.workDay.time - timeSpent)*1000).toString()}</div>
-
 					</div>
 				}
 			</div>
