1a813e08ceabc910737113a13836d685a2f25119
Jonathan Cook <jonathan@stratosphere.digital>
2017-11-27 11:44:21 -0500

    .

--- src/WorkDay.js
+++ src/WorkDay.js
@@ -8,32 +8,84 @@ import _ from 'lodash';
 export class WorkDay extends XComponent {
 	constructor() {
 		super({
+			actions: {
+				deleteEntry() {
+
+				}
+			}
+		});
+	}
+
+	log() {
+		var end = this.props.workDay.end || new Date();
+		return db.work_log_entries.filter((workLogEntry) => {
+			return workLogEntry.start.isBetween(this.props.workDay.begin, end);
 		});
 	}
 	xRender() {
-		var end = this.props.workDay.end || new Date();
-		var timeSpent = Math.floor(db.work_log_entries.filter((workLogEntry) => {
-			return workLogEntry.start.isBetween(this.props.workDay.begin, end);
-		}).reduce((timeSpent, entry) => {
+		var timeSpent = Math.floor(this.log().reduce((timeSpent, entry) => {
 			return timeSpent + (entry.end || new Date()).getTime() - entry.start.getTime();
 		}, 0)/1000);
 		var timeSinceStarted = this.props.workDay.begin ? Math.floor((new Date().getTime() - this.props.workDay.begin.getTime())/1000) : 0;
+		var distractionTime = timeSinceStarted - timeSpent;
 		return (
 			<div>
 				<div>Begin: <PropertyField object={this.props.workDay} property="begin" type="datetime" /></div>
 				<div>End: <PropertyField object={this.props.workDay} property="end" type="datetime" /></div>
 				<div>Time: <PropertyField object={this.props.workDay} property="time" type="duration" /></div>
+				<div>Distraction Time: <PropertyField object={this.props.workDay} property="distractionTime" type="duration" /></div>
 
-				<div>{juration.stringify(timeSpent)}</div>
+				<div>Time Spent Working: {juration.stringify(timeSpent)}</div>
 				<div>Time Since Started: {juration.stringify(timeSinceStarted)}</div>
-				<div>Other Time: {juration.stringify(timeSinceStarted - timeSpent)}</div>
+				{this.props.workDay.distractionTime ? <div>
+					<div>Distraction Time: {juration.stringify(distractionTime)}/{juration.stringify(this.props.workDay.distractionTime)} ({juration.stringify(this.props.workDay.distractionTime - distractionTime)})</div>
+				</div> : <div>Distraction Time: {juration.stringify(distractionTime)}</div>}
 
 				{this.props.workDay.time &&
 					<div>
 						<div>Time left: {juration.stringify(this.props.workDay.time - timeSpent)}</div>
-						<div>Time When Finished: {new Date(new Date().getTime() + (this.props.workDay.time - timeSpent)*1000).toString()}</div>
+						<div>Time When Finished: {new Date(new Date().getTime() + ((this.props.workDay.time - timeSpent) + (this.props.workDay.distractionTime ? Math.max(0, this.props.workDay.distractionTime - distractionTime) : 0))*1000).toString()}</div>
 					</div>
 				}
+
+				<h2>Log</h2>
+				<ul>
+					{this.log().map(entry => {
+						return (
+							<li key={entry._id}>
+								<div>
+									<label>Subject: </label>
+									<PropertyField type="entity" object={entry} property="subject" />
+								</div>
+								<div>
+									<label>Description: </label>
+									<PropertyField type="text" object={entry} property="description" />
+								</div>
+								<div>
+									<label>Start: </label>
+									<PropertyField type="datetime" object={entry} property="start" />
+								</div>
+								<div>
+									<label>End: </label>
+									<PropertyField type="datetime" object={entry} property="end" />
+								</div>
+								<div>
+									<label>Activity: </label>
+									<PropertyField type="text" object={entry} property="activity.activity" />
+								</div>
+
+								{entry.activity.object.entity && <div>
+									<label>Object Entity: </label>
+									<PropertyField type="entity" object={entry} property="activity.object.entity" />
+								</div>}
+
+								<button onClick={this.actions.deleteEntry.bind(entry)}>Delete</button>
+							</li>
+						);
+					})}
+				</ul>
+
+
 			</div>
 		);
 	}
